4f5747ae2c017a7ed00286793550ba9e20e6a671
Evrayem3201b <154753963+Evrayem3201b@users.noreply.github.com>
2025-09-21 16:19:01 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,21 +1,9 @@
-import course from './course'
-import assignments from './assignment'
-import teacherProfile from './teacher'
-import lesson from './lesson'
-import quiz from './quiz'
-import quizQusetion from './quizQusetion'
+
 import openQuestion from './openQuestion'
 import blockContent from './blockContent'
-import {alignedBlock} from './alignedBlock'
+
 
 export const schemaTypes = [
-  course,
-  assignments,
-  lesson,
-  teacherProfile,
-  quiz,
-  quizQusetion,
   openQuestion,
-  blockContent,
-  alignedBlock,
+  blockContent, // you must add it here to be able to use it
 ]
